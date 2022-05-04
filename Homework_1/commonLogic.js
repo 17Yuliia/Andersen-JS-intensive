6eaf102acd1b167ebd6db17c45f6f508a5a6b682
17Yuliia <ahalakova1701@gmail.com>
2022-05-04 19:53:05 +0200

    all files deleted

--- Homework_1/commonLogic.js
+++
@@ -1,9 +0,0 @@
-const getData = (message) => {
-    return prompt(message); 
-}
-
-const validateNumber = (input) => {
-    const number = Number(input);
-
-    return Number.isFinite(number) && input !== '';
-}
