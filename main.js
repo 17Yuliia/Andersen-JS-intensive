cd7633abcca2edff7ea29f7402b4a0c6c668e8c8
17Yuliia <ahalakova1701@gmail.com>
2022-05-11 17:57:21 +0200

    homework-3 is done

--- main.js
+++ main.js
@@ -1,80 +1,23 @@
-// TASK 1
-const makeObjectDeepCopy = (obj) => {
-    const isObject = typeof obj === 'object';
+Array.prototype.myFilter = function(callback, array = this) {
+    const filteredArray = [];
 
-    if (!isObject || obj === null) {
-        return obj;
+    for (let i = 0; i < array.length; i++) {
+        const isFiltered = callback(array[i], i, array);
+
+        if (isFiltered) {
+            filteredArray.push(array[i]);
+        }
     }
 
-    const objCopy = new obj.constructor();
-
-    Object.entries(obj).forEach(([key, value]) => {
-        objCopy[key] = makeObjectDeepCopy(value);
-    });
-
-    return objCopy;
+    return filteredArray;
 }
 
-const isValidNumber = (number) => {
-    return !isNaN(number) && typeof number === 'number';
-}
+function createDebounceFunction(callback, timeout) {
+    let timeoutId;
 
-//TASK 2
-const selectFromInterval = (arr, from, to) => {
-    if (!Array.isArray(arr)) {
-        throw new Error('Первый параметр не является массивом!');
-    }
+    return (...args) => {
+        clearTimeout(timeoutId);
 
-    const isValidValues = isValidNumber(from) && isValidNumber(to);
-
-    if (!isValidValues) {
-        throw new Error('Индекс должен быть целым числом!');
-    }
-
-    const lowerBound = Math.min(from, to);
-    const higherBound = Math.max(from, to);
-
-    const filteredArr = arr.filter((number) => {
-        if (isValidNumber(number)) {
-            throw new Error('Массив должен состоять из чисел!');
-        }
-
-        const numberIsInRange = number >= lowerBound && number <= higherBound;
-
-        return numberIsInRange;
-    })
-
-    return filteredArr;
-}
-
-//TASK3
-const myIterable = {
-    from: 17,
-    to: 22,
-
-    [Symbol.iterator]() {
-        const isValidValues = isValidNumber(this.from) && isValidNumber(this.to);
-        const fromIsLowerThanTo = this.from <= this.to;
-
-        if (!isValidValues || !fromIsLowerThanTo) {
-            throw new Error('Поле должно быть числом!');
-        }
-
-        let counter = this.from;
-
-        return {
-            next: () => {
-                if (counter <= this.to) {
-                    return {
-                        value: counter++,
-                        done: false,
-                    }
-                }
-
-                return {
-                    done: true,
-                }
-            }
-        }
+        timeoutId = setTimeout(() => callback(...args), timeout);
     }
 }
